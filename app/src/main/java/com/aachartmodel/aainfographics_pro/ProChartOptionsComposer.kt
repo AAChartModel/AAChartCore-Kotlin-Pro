26ae92c837f9789a5381ac9a8463203f29a0a36b
An An <2236368544@qq.com>
2020-12-16 18:35:23 +0800

    Update ProChartOptionsComposer.kt

--- app/src/main/java/com/aachartmodel/aainfographics_pro/ProChartOptionsComposer.kt
+++ app/src/main/java/com/aachartmodel/aainfographics_pro/ProChartOptionsComposer.kt
@@ -16,18 +16,18 @@ public class ProChartOptionsComposer {
                     .title(AATitle()
                             .text("AAChartKit-Pro 桑基图"))
                     .colors(arrayOf(
-                            "rgb(137,78,36)",
-                            "rgb(220,36,30)",
-                            "rgb(255,206,0)",
-                            "rgb(1,114,41)",
-                            "rgb(0,175,173)",
-                            "rgb(215,153,175)",
-                            "rgb(106,114,120)",
-                            "rgb(114,17,84)",
-                            "rgb(0,0,0)",
-                            "rgb(0,24,168)",
-                            "rgb(0,160,226)",
-                            "rgb(106,187,170)"
+                            AAColor.rgbaColor(137,78,36),
+                            AAColor.rgbaColor(220,36,30),
+                            AAColor.rgbaColor(255,206,0),
+                            AAColor.rgbaColor(1,114,41),
+                            AAColor.rgbaColor(0,175,173),
+                            AAColor.rgbaColor(215,153,175),
+                            AAColor.rgbaColor(106,114,120),
+                            AAColor.rgbaColor(114,17,84),
+                            AAColor.rgbaColor(0,0,0),
+                            AAColor.rgbaColor(0,24,168),
+                            AAColor.rgbaColor(0,160,226),
+                            AAColor.rgbaColor(106,187,170)
                     ))
                     .series(arrayOf(
                             AASeriesElement()
@@ -232,7 +232,7 @@ public class ProChartOptionsComposer {
                                     .data(AAOptionsData.heatmapData)
                                     .dataLabels(AADataLabels()
                                             .enabled(true)
-                                            .color(AAColor.redColor()))))
+                                            .color(AAColor.Red))))
         }
 
 
