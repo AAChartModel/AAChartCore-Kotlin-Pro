035ac2742ad764926fbf2e855ee5aecc8bd8f008
AnAn <2236368544@qq.com>
2021-01-19 16:32:39 +0800

    Update AATextPath.kt

--- aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodelpro/AATextPath.kt
+++ aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodelpro/AATextPath.kt
@@ -2,14 +2,14 @@ package com.aachartmodel.aacharts.aaoptionsmodelpro
 
 class AATextPath {
     var enabled = false
-    var attributes: Map<*, *>? = null
+    var attributes: Map<String, *>? = null
 
     fun enabled(prop: Boolean): AATextPath {
         enabled = prop
         return this
     }
 
-    fun attributes(prop: Map<*, *>?): AATextPath {
+    fun attributes(prop: Map<String, *>?): AATextPath {
         attributes = prop
         return this
     }
